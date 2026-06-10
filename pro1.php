f640add7957499db6d76a6dba5734ba49de75008
meetdabhi127640-cloud <meet.dabhi127640@marwadiuniversity.ac.in>
2026-06-10 13:02:45 +0530

    Add files via upload

---
+++ pro1.php
@@ -0,0 +1,15 @@
+<?php 
+      const university = "gujarat univrsity";
+
+       $name = "meet dabhi";
+       $semester = 5;
+       $spi = 6.66;
+       $result = "pass";
+
+       echo"--------sem 5-----------"."<br>";
+       echo"univrsity :".university."<br>";
+       echo "student name :".$name."<br>";
+       echo"semester :".$semester."<br>";
+       echo"spi :".$spi."<br>";
+       echo"result :".$result."<br>";
+?>
