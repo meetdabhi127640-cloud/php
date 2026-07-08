c2d8fe4f286da35aca3e14a19ce48226485fae9a
meetdabhi127640-cloud <meet.dabhi127640@marwadiuniversity.ac.in>
2026-07-08 10:19:02 +0530

    Add files via upload

---
+++ program2.4.php
@@ -0,0 +1,10 @@
+<?php
+
+    $name = "dabhi meet";
+    echo strlen($name)."<br>";
+    echo strrev($name)."<br>";
+    echo strtolower($name)."<br>";
+    echo strtoupper($name)."<br>";
+    echo strpos($name,"a",0)."<br>";
+    echo str_word_count($name);
+?>
